ab346629006692b3f7196edb3904add9e24a125d
Дмитрий <dmitriy@MacBook-Pro-3.local>
2020-12-10 15:42:35 +0300

    Tokens files

---
+++ build/ios/StyleDictionaryColor.h
@@ -0,0 +1,26 @@
+
+//
+// StyleDictionaryColor.h
+//
+// Do not edit directly
+// Generated on Thu, 10 Dec 2020 12:42:14 GMT
+//
+
+#import <UIKit/UIKit.h>
+
+
+typedef NS_ENUM(NSInteger, StyleDictionaryColorName) {
+ColorBaseGrayLight,
+ColorBaseGrayMedium,
+ColorBaseGrayDark,
+ColorBaseRed,
+ColorBaseGreen,
+ColorFontBase,
+ColorFontSecondary,
+ColorFontTertiary
+};
+
+@interface StyleDictionaryColor : NSObject
++ (NSArray *)values;
++ (UIColor *)color:(StyleDictionaryColorName)color;
+@end
